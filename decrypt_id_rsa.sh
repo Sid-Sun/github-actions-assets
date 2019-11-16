326fac0ddc6724f99a078cdbd235afef881d502a
Sid Sun <sid@sidsun.com>
2019-11-16 23:06:45 +0530

    Fix: Fix script
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- decrypt_id_rsa.sh
+++ decrypt_id_rsa.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/gh_actions_secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/gh_actions_secrets/secret.asc gpg_key.asc.gpg
+--output $HOME/gh_actions_secrets/id_rsa id_rsa.gpg
