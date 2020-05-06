28feb6cfa6b7c926cc010b9247efceceb8864700
Sidharth Soni (Sid Sun) <sid@sidsun.com>
2020-05-06 13:42:12 +0530

    Add scripts for seat & seal
    Signed-off-by: Sidharth Soni (Sid Sun) <sid@sidsun.com>

---
+++ seal-push-to-hosting.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+UDIR=/srv/www/cdn/seal-256-cfb
+ssh-keyscan -p $PORT $HOST >> ~/.ssh/known_hosts
+
+echo "cd $UDIR
+put debian-package/seal-256-cfb_amd64.deb seal-256-cfb_amd64.deb
+put /built-binaries/* .
+bye" > batchfile
+#Let us upload
+sftp -b batchfile -P $PORT $USER@$HOST
---
+++ seat-push-to-hosting.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+UDIR=/srv/www/cdn/seat-256-cfb
+ssh-keyscan -p $PORT $HOST >> ~/.ssh/known_hosts
+
+echo "cd $UDIR
+put debian-package/seat-256-cfb_amd64.deb seat-256-cfb_amd64.deb
+put /built-binaries/* .
+bye" > batchfile
+#Let us upload
+sftp -b batchfile -P $PORT $USER@$HOST
