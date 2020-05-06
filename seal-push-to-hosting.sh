16b506a1079f4a7dc62524061b90f316c7a8e5e6
Sidharth Soni (Sid Sun) <sid@sidsun.com>
2020-05-06 14:12:31 +0530

    Fix seal & seat scripts
    Signed-off-by: Sidharth Soni (Sid Sun) <sid@sidsun.com>

--- seal-push-to-hosting.sh
+++ seal-push-to-hosting.sh
@@ -5,7 +5,7 @@ ssh-keyscan -p $PORT $HOST >> ~/.ssh/known_hosts
 
 echo "cd $UDIR
 put debian-package/seal-256-cfb_amd64.deb seal-256-cfb_amd64.deb
-put /built-binaries/* .
+put built-binaries/* .
 bye" > batchfile
 #Let us upload
 sftp -b batchfile -P $PORT $USER@$HOST
--- seat-push-to-hosting.sh
+++ seat-push-to-hosting.sh
@@ -5,7 +5,7 @@ ssh-keyscan -p $PORT $HOST >> ~/.ssh/known_hosts
 
 echo "cd $UDIR
 put debian-package/seat-256-cfb_amd64.deb seat-256-cfb_amd64.deb
-put /built-binaries/* .
+put built-binaries/* .
 bye" > batchfile
 #Let us upload
 sftp -b batchfile -P $PORT $USER@$HOST
